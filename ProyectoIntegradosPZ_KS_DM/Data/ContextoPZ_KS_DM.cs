8f5658356b79a106a514a9f877aa8d637a7f5748
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-04-28 10:25:56 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoIntegradosPZ_KS_DM/Data/ContextoPZ_KS_DM.cs
@@ -0,0 +1,14 @@
+﻿namespace ProyectoIntegradosPZ_KS_DM.Data
+{
+    public class ContextoPZ_KS_DM
+    {
+        public string? Conexion { get; }
+
+        public ContextoPZ_KS_DM(string valor)
+        {
+            Conexion = valor;
+        }
+    
+    
+    }
+}
---
+++ ProyectoIntegradosPZ_KS_DM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoIntegradosPZ_KS_DM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
